b4b48e1a4c54105cc16f304d7c3936e5af4cbfb1
SexyDoGyun <kimdk0903@naver.com>
2025-06-15 01:25:44 +0900

    feat: 회원가입 요청/응답 Dto 구현

---
+++ src/main/java/com/taskflow/domain/member/dto/MemberSignupRequestDto.java
@@ -0,0 +1,30 @@
+package com.taskflow.domain.member.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+
+@Getter
+public class MemberSignupRequestDto {
+
+    @NotBlank(message = "사용자 닉네임은 필수로 입력해주세요.")
+    private String username;
+
+    @NotBlank(message = "이메일은 필수로 입력해주세요.")
+    @Email
+    private String email;
+
+    @NotBlank(message = "비밀번호는 필수로 입력해주세요.")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()\\-_=+\\[{\\]};:'\",<.>/?]).{8,}$",
+            message = "비밀번호는 최소 8글자 이상, 대소문자 포함 영문 + 숫자 + 특수문자를 최소 1글자씩 포함해야합니다."
+    )
+    private String password;
+
+    @NotBlank(message = "사용자 이름은 필수로 입력해주세요.")
+    private String name;
+
+    @NotBlank
+    private String userRole;
+}
