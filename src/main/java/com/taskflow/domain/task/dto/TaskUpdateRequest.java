7f0db3d8c0e1bd754eb104ad135147c5f6534651
Gyum1 <xoruadl123@gmail.com>
2025-06-16 19:25:12 +0900

    feat: TaskUpdateRequest에 startDate, dueDate 필드 추가

--- src/main/java/com/taskflow/domain/task/dto/TaskUpdateRequest.java
+++ src/main/java/com/taskflow/domain/task/dto/TaskUpdateRequest.java
@@ -4,9 +4,11 @@ import com.taskflow.domain.task.enums.TaskPriority;
 import com.taskflow.domain.task.enums.TaskStatus;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 /**
  * 일정 수정 요청을 위한 DTO 클래스
- * 클라이언트로부터 수정할 일정 정보를 전달받음
+ * 클라이언트로부터 일정 수정 요청 데이터를 수신
  */
 @Getter
 public class TaskUpdateRequest {
@@ -14,6 +16,9 @@ public class TaskUpdateRequest {
     private String title;
     private String description;
     private TaskPriority priority;
-    private String managerName;
     private TaskStatus status;
+    private String managerName;
+
+    private LocalDateTime startDate;
+    private LocalDateTime dueDate;
 }
